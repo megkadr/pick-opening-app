17c53588a168e9bc26a8e807b5ca1dfd50c07525
Artur <a.rezmer@sellpander.pl>
2024-07-26 17:40:28 +0200

    #Added prod api

--- AniPick/src/utils/RequestServices/AxiosClient.tsx
+++ AniPick/src/utils/RequestServices/AxiosClient.tsx
@@ -4,7 +4,7 @@
 import axios from "axios";
 
 const localApiUrl = "https://localhost:44324";
-const productionApiUrl = "https://localhost:44324";
+const productionApiUrl = "https://anipickapi.azurewebsites.net/";
 
 export const axiosClient = axios.create({
     baseURL: import.meta.env.DEV ? localApiUrl : productionApiUrl,
