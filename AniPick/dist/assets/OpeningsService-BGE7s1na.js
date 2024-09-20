a4b5def24220ea405845822a6282051a41da6110
Artur <a.rezmer@sellpander.pl>
2024-09-20 03:05:52 +0200

    #Last production build

--- AniPick/dist/assets/OpeningsService-BGE7s1na.js
+++
@@ -1,1 +0,0 @@
-import{f as e}from"./index-Dame-ujr.js";async function t(n){return await e.post("/Openings/opening",n)}async function i(n){return(await e.get(`/Openings/openings?year=${n}`)).data}async function o(){return(await e.get("/Openings/allOpenings")).data}export{t as a,o as b,i as g};
---
+++ AniPick/dist/assets/OpeningsService-DE2fLeBT.js
@@ -0,0 +1,1 @@
+import{f as e}from"./index-A4M8k-aT.js";async function t(n){return await e.post("/Openings/opening",n)}async function i(n){return(await e.get(`/Openings/openings?year=${n}`)).data}async function o(){return(await e.get("/Openings/allOpenings")).data}export{t as a,o as b,i as g};
